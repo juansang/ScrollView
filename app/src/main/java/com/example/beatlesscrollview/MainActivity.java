8f9f1369af73feb2b974b54026f004b9d3cae7d7
juansanchezg <jsanchezgayoso@cifpfbmoll.eu>
2022-10-29 13:25:03 +0200

    Task13.4

--- app/src/main/java/com/example/beatlesscrollview/MainActivity.java
+++ app/src/main/java/com/example/beatlesscrollview/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.beatlesscrollview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button comButton;
+    private Button saveButton;
+    private String comment;
+    private EditText comment_box;
+    private TextView commentTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        comment_box = (EditText) findViewById(R.id.comment_box);
+        comment_box.setVisibility(View.INVISIBLE);
+
+        saveButton = findViewById(R.id.addText_button);
+        saveButton.setVisibility(View.INVISIBLE);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                comment_box.setVisibility(View.INVISIBLE);
+                writeComment();
+            }
+        });
+
+        comButton = findViewById(R.id.comment_button);
+        comButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                comment_box.setVisibility(View.VISIBLE);
+                saveButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+    }
+    public void writeComment(){
+        comment="Your comment: " + comment_box.getText().toString();
+        commentTextView = findViewById(R.id.comment_text);
+        commentTextView.setText(comment);
+        comButton.setVisibility(View.INVISIBLE);
+        comment_box.setVisibility(View.INVISIBLE);
+        saveButton.setVisibility(View.INVISIBLE);
     }
 }
